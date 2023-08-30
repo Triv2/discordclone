01f2d01051d162c596a6b46fb418f59639432774
Trivial-lore <triviallore@gmail.com>
2023-08-30 07:42:30 -0500

    navigation item added

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
 
 
 
@@ -31,8 +32,13 @@ export const NavigationSideBar = async () => {
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"/>
       <ScrollArea className="flex-1 w-full">
         {servers.map((server) => (
-          <div key={server.id}>
-            {server.name}
+          <div className="mb-4"
+          key={server.id}>
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
           </div>
         ))}
 
