0a5b53539b2d4e5775d1aa0337952d8ddc87003f
Trivial-lore <triviallore@gmail.com>
2023-08-29 22:33:41 -0500

    scroll-area added

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,8 @@ import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { NavigationAction } from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 
 
@@ -26,6 +28,15 @@ export const NavigationSideBar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction/>
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"/>
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id}>
+            {server.name}
+          </div>
+        ))}
+
+      </ScrollArea>
     </div>
   );
 }
