4b2ba08812521d6be51f136cb267eb8c8ae287c6
Trivial-lore <triviallore@gmail.com>
2023-08-30 19:43:19 -0500

    added types and server header

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,9 +1,10 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-interface ServerIdLayoutProps {}
+
 
 const ServerIdLayout = async ({
   children,
@@ -36,7 +37,7 @@ const ServerIdLayout = async ({
   return (
     <div className="h-full">
       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        Server Sidebar
+        <ServerSidebar serverId={params.serverId}/>
       </div>
       <main className="h-full md:pl-60">
       {children}
---
+++ components/server/server-header.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { ServerWithMembersWithProfiles } from "@/types"
+import { MemberRole } from "@prisma/client";
+
+
+interface ServerHeaderProps {
+  server: ServerWithMembersWithProfiles
+  role?:MemberRole;
+}
+
+export const ServerHeader = ({ 
+  server, 
+  role 
+}: ServerHeaderProps) => {
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerator = isAdmin || role === MemberRole.MODERATOR;
+  return(
+    <div>
+      Server Header
+    </div>
+  )
+}
