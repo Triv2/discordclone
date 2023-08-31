92c1e1f807a176cacb91af0e658fc6a524d5db7f
Trivial-lore <triviallore@gmail.com>
2023-08-31 10:29:58 -0500

    updating modal to invite users

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { CreateServerModal } from "@/components/modals/create-server-modal"
 import {useState, useEffect} from'react'
+import { InviteModal } from "@/components/modals/invite-modal";
 
 
 
@@ -19,6 +20,7 @@ return null;
   return (
     <>
      <CreateServerModal />
+     <InviteModal />
     </>
   );
 }
