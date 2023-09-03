8ec612444d5db519a5b561ce050753caf1aab799
Trivial-lore <triviallore@gmail.com>
2023-09-02 23:04:43 -0500

    channels route finished

--- app/api/channels/route.ts
+++ app/api/channels/route.ts
@@ -1,4 +1,6 @@
 import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { MemberRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function POST(
@@ -17,6 +19,35 @@ export async function POST(
     if(!serverId){
       return new NextResponse("Server ID missing", { status: 400 });
     }
+    if(name=== "general"){
+      return new NextResponse("Name cannot be general", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: { 
+        id: serverId,
+      members: {
+        some:{
+          profileId: profile.id,
+          role:{
+            in: [MemberRole.ADMIN,MemberRole.MODERATOR]
+          }
+        }
+      }
+      },
+      data:{
+        channels: {
+            create:{
+              profileId: profile.id,
+              name,
+              type,
+            }
+          }
+        }
+      });
+
+      return NextResponse.json(server);
+
   } catch (error){
     console.log("[CHANNELS_POST]", error);
     return new NextResponse("Internal Error", {status: 500});
