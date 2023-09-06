f986081a0e8849ac48582b6576a0834a06737f2f
Trivial-lore <triviallore@gmail.com>
2023-09-05 21:03:41 -0500

    fixed again

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -3,7 +3,8 @@
 import qs from "query-string"
 import axios from 'axios'
 import * as z from 'zod';
-import {zodResolver} from '@hookform/resolvers/zod'
+import {zodResolver} from '@hookform/resolvers/zod' 
+
 import { useForm } from 'react-hook-form'
 
 import { Select, SelectContent, SelectItem,SelectTrigger,SelectValue} from "@/components/ui/select"
