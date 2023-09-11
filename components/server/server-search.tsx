9c4f3c4f6d66a28d63dc3021bcd8102ee0e7d069
Trivial-lore <triviallore@gmail.com>
2023-09-11 16:04:58 -0500

    added on click to server search

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -2,6 +2,7 @@
 import { Search } from 'lucide-react';
 import {useState, useEffect} from'react'
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
+import { useParams, useRouter } from 'next/navigation';
 
 interface ServerSearchProps {
   data:{
@@ -18,6 +19,8 @@ interface ServerSearchProps {
 export const ServerSearch= ({ data }:ServerSearchProps) => {
   
   const [open, setOpen] = useState(false);
+  const router= useRouter();
+  const params = useParams();
   
   useEffect(() => {
    const down = (e:KeyboardEvent) => {
@@ -30,6 +33,17 @@ export const ServerSearch= ({ data }:ServerSearchProps) => {
    document.addEventListener("keydown", down);
    return () => document.removeEventListener("keydown", down);
   }, []);
+
+  const onClick = ({ id, type}: {id:string, type:"channel" | "member" }) => {
+    setOpen(false);
+
+    if(type==="member") {
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`)
+    }
+    if (type === "channel") {
+      return router.push(`/servers/${params?.serverId}/channels/${id}`)
+    }
+  };
 
   return (
     <>
@@ -55,7 +69,7 @@ export const ServerSearch= ({ data }:ServerSearchProps) => {
               <CommandGroup key={label} heading={label}>
                 {data?.map(({id,icon,name})=>{
                   return(
-                    <CommandItem key={id}>
+                    <CommandItem key={id} onSelect={()=>onClick({id,type})}>
                       {icon}
                       <span>{name}</span>
                     </CommandItem>
