0bbaec2e776ece8107b61e377b80d149183d4969
Trivial-lore <triviallore@gmail.com>
2023-09-11 16:36:22 -0500

    server section added

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -2,6 +2,8 @@
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ChannelType, MemberRole } from "@prisma/client";
+import { ActionTooltip } from "../action-tooltip";
+import { Plus } from "lucide-react";
 
 interface ServerSectionProps{
   label:string;
@@ -19,8 +21,17 @@ export const ServerSection = ({
   server
 }:ServerSectionProps) => {
   return (
-    <div>
-      <h1>Server Section</h1>
+    <div className="flex items-center justify-between py-2">
+      <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+        {label}
+        </p>
+        {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionTooltip label= "Create Channel" side="top">
+          <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc--300 transition">
+            <Plus className="h-4 w-4"/>
+          </button>
+        </ActionTooltip>
+        )}
     </div>
   )
 }
