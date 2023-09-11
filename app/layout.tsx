4783c765f408d1bbf8890bf2f4ffed485d3886af
Trivial-lore <triviallore@gmail.com>
2023-09-11 08:43:24 -0500

    updating server search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { cn } from '@/lib/utils'
 import { Open_Sans } from 'next/font/google'
+
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
-import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
 
 
--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Search } from 'lucide-react';
 import {useState, useEffect} from'react'
 
 interface ServerSearchProps {
@@ -25,8 +26,13 @@ if (!isMounted) {
 return null;
 }
   return (
-    <div>
-      ServerSearch
-    </div>
+    <>
+    <button
+      className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+    >
+      <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400"/>
+      <p>Search</p>
+    </button>
+    </>
   );
 }
