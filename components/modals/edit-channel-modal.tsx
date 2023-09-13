40b277f77ebc9b2842f837c6baa3e1b4993155d3
Trivial-lore <triviallore@gmail.com>
2023-09-13 09:59:48 -0500

    edit channel working

--- components/modals/edit-channel-modal.tsx
+++ components/modals/edit-channel-modal.tsx
@@ -61,7 +61,7 @@ export const EditChannelModal = () => {
           serverId: server?.id
         }
       });
-      await axios.post(url, values);
+      await axios.patch(url, values);
       
       form.reset();
       router.refresh();
