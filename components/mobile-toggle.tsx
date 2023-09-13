893c2af31af7a5782b9411ad35c49d4048059f40
Trivial-lore <triviallore@gmail.com>
2023-09-13 13:20:17 -0500

    created mobile-toggle button

---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,16 @@
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger,} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+
+export const MobileToggle = () => {
+  return (
+  <Sheet>
+    <SheetTrigger asChild>
+      <Button>
+
+      </Button>
+    </SheetTrigger>
+  </Sheet>
+  )
+}
