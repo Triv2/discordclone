c15e99429715376d2e3b43512d8f888b4932178d
Trivial-lore <triviallore@gmail.com>
2023-09-16 09:56:57 -0500

    u

--- components/modals/message-file-modal.tsx
+++ components/modals/message-file-modal.tsx
@@ -4,11 +4,12 @@ import axios from 'axios'
 import * as z from 'zod';
 import {zodResolver} from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import {useState, useEffect} from'react'
+import qs from 'query-string';
+
 
 import { Dialog, DialogContent, DialogFooter, DialogDescription,DialogHeader,DialogTitle} from "@/components/ui/dialog"
-import { Form, FormControl, FormField, FormItem, FormLabel,FormMessage} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
+import { Form, FormControl, FormField, FormItem, } from '@/components/ui/form'
+
 import { Button } from '@/components/ui/button'
 import { FileUpload } from '@/components/file-upload';
 import { useRouter } from 'next/navigation';
@@ -18,7 +19,7 @@ import { useModal } from '@/hooks/use-modal-store';
 
 const formSchema = z.object({
  
-  imageUrl: z.string().min(1,{message: 'Server image is required'}),
+  fileUrl: z.string().min(1,{message: 'Attachment is required'}),
 
 })
 
@@ -29,12 +30,13 @@ export const MessageFileModal = () => {
   const router=useRouter();
 
   const isModalOpen = isOpen && type === "messageFile";
+  const {apiUrl, query} = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
      
-      imageUrl: '',
+      fileUrl: '',
     }
   });
 
@@ -47,11 +49,20 @@ export const MessageFileModal = () => {
 
   const onSubmit= async (values: z.infer<typeof formSchema>)=> {
     try{
-      await axios.post('/api/servers', values);
-      console.log("axios sucks");
+
+      const url = qs.stringifyUrl({
+        url:apiUrl || "",
+        query,
+      });
+
+      await axios.post(url, {
+        ...values,
+        conent: values.fileUrl,
+       });
+      
       form.reset();
       router.refresh();
-      window.location.reload();
+      handleClose();
     }
     catch(error){
       console.log(error)
@@ -76,7 +87,7 @@ export const MessageFileModal = () => {
               <div className="flex items-center justify-center text-center">
                 <FormField 
                   control={form.control}
-                  name="imageUrl"
+                  name="fileUrl"
                   render={({field}) => (
                     <FormItem>
                       <FormControl>
