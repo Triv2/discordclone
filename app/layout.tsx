b4703686d27bc22d08d547bc4925c87c855eb3cc
Trivial-lore <triviallore@gmail.com>
2023-09-25 17:25:42 -0500

    use chat query hook in-progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 
 
 const font = Open_Sans({ subsets: ['latin'] })
@@ -27,8 +28,10 @@ export default function RootLayout({
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">
             <SocketProvider>
-          <ModalProvider />
-            {children}
+              <ModalProvider />
+                <QueryProvider>
+                  {children}
+                </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
