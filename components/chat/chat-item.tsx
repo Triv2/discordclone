1203ac1a8bac08ff955ff94232bfb653954f83dd
Trivial-lore <triviallore@gmail.com>
2023-09-26 15:44:56 -0500

    updating chat-item

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,12 +1,100 @@
 'use client'
 
-interface ChatItemProps {
+import * as z from "zod";
+import axios from "axios";
+import qs from "query-string";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Member, MemberRole, Profile } from "@prisma/client";
+import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useRouter, useParams } from "next/navigation";
 
+import { UserAvatar } from "@/components/user-avatar";
+import { ActionTooltip } from "@/components/action-tooltip";
+import { cn } from "@/lib/utils";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useModal } from "@/hooks/use-modal-store";
+
+interface ChatItemProps {
+  id:string;
+  content:string;
+  member:Member & {
+    profile: Profile;
+  };
+  timestamp: string;
+  fileUrl:string | null;
+  deleted:boolean;
+  currentMember:Member;
+  isUpdated:boolean;
+  socketUrl: string;
+  socketQuery: Record <string,string>;
 }
 
+const roleIconMap= {
+  "GUEST": null,
+  "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500"/>,
+  "ADMIN": <ShieldAlert className="h-4 w-4 ml-2 text-rose-500"/>,
+}
+
+const formSchema= z.object({
+  content:z.string().min(1),
+})
+
 export const ChatItem = ({
-  
+  id,
+  content,
+  member,
+  timestamp,
+  fileUrl,
+  deleted,
+  isUpdated,
+  socketUrl,
+  socketQuery,
+  currentMember,
 }:ChatItemProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const {onOpen } = useModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const onMemberClick = () => {
+    if(member.id === currentMember.id){
+      return;
+    } 
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event:any) => {
+      if(event.key=== "Escape" || event.keyCode === 27){
+        setIsEditing(false);
+      }
+  };
+  window.addEventListener("keydown", handleKeyDown);
+  return () => window.removeEventListener("keydown", handleKeyDown);
+}, []);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: content,
+    },
+  })
+
+  const isLoading = form.formState.isSubmitting;
+
+  
+
+
   return(
     <div>
       chatitem
